5e1a49b794c8006102ce9fdcc17ddf35e85e5c05
acfabro <acfabro@gmail.com>
2020-02-17 07:33:23 +0800

    uses pre-filled db dump

--- database/database.sql
+++ database/database.sql
@@ -7,7 +7,7 @@
 #
 # Host: 192.168.10.10 (MySQL 5.7.28-0ubuntu0.18.04.4)
 # Database: mailerlite
-# Generation Time: 2020-02-15 08:46:45 +0000
+# Generation Time: 2020-02-16 23:31:32 +0000
 # ************************************************************
 
 
@@ -37,6 +37,17 @@ CREATE TABLE `fields` (
   KEY `subscriber_id_fk` (`subscriber_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+LOCK TABLES `fields` WRITE;
+/*!40000 ALTER TABLE `fields` DISABLE KEYS */;
+
+INSERT INTO `fields` (`id`, `title`, `type`, `value`, `subscriber_id`, `created_at`, `updated_at`)
+VALUES
+	(20,'Nickname','string','The tattler',1,'2020-02-16 23:30:31','2020-02-16 23:30:31'),
+	(21,'DOB','date','1979-01-01',1,'2020-02-16 23:30:31','2020-02-16 23:30:31'),
+	(22,'Yogurts per hour','number','5',3,'2020-02-16 23:30:52','2020-02-16 23:30:52');
+
+/*!40000 ALTER TABLE `fields` ENABLE KEYS */;
+UNLOCK TABLES;
 
 
 # Dump of table subscribers
@@ -54,6 +65,17 @@ CREATE TABLE `subscribers` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+LOCK TABLES `subscribers` WRITE;
+/*!40000 ALTER TABLE `subscribers` DISABLE KEYS */;
+
+INSERT INTO `subscribers` (`id`, `name`, `email`, `state`, `created_at`, `updated_at`)
+VALUES
+	(1,'Jake Peralta','jake@gmail.com','unconfirmed','2020-02-16 12:02:11','2020-02-16 12:02:11'),
+	(3,'Terry Jeffords','terry@nine-nine.com','active','2020-02-16 12:04:42','2020-02-16 12:04:42'),
+	(4,'Charles Boyle','charles@gmail.com','active','2020-02-16 12:17:42','2020-02-16 12:20:23');
+
+/*!40000 ALTER TABLE `subscribers` ENABLE KEYS */;
+UNLOCK TABLES;
 
 
 
